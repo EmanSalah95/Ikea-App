5c0efdd411c1199993071080a3b9b0782c812ff8
raghda244 <raghdamohsen244@gmail.com>
2022-03-04 17:53:28 +0200

    add product slidder in product page

--- src/screens/Product.js/product.js
+++ src/screens/Product.js/product.js
@@ -1,19 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { getDocumentByID } from '../../services/firebase';
+import { SwiperFlatList } from 'react-native-swiper-flatlist';
+import { h, w } from '../../constants/dimentions';
 
-export default function Product() {
+export default function Product({ route }) {
+  const colors = ['tomato', 'thistle', 'skyblue', 'teal'];
+  const [product, setProduct] = useState({});
+  const getProduct = () => {
+    getDocumentByID('Products', route.params.id).then((res) => {
+      setProduct(res);
+    })
+      .catch((err) => console.log('error :', err));
+  }
+  useEffect(() => getProduct(), []);
   return (
     <View style={styles.container}>
-      <Text>Product</Text>
-      <Text>Product</Text>
+      <View>
+        <SwiperFlatList
+          index={0}
+          showPagination
+          data={product.Images}
+          renderItem={({ item }) => (
+            <Image source={{ uri: item }} style={styles.ImageSize} />
+          )}
+          paginationStyle={styles.pagination}
+          // PaginationComponent={()=>{return <View style={{height:2,backgroundColor:'black'}}></View>}}
+          paginationActiveColor='grey'
+          paginationDefaultColor='silver'
+        />
+      </View>
+      <Text>HELLOOOOOOOOO</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+    container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height:h
   },
+  ImageSize: {
+    width: w,
+    height: h * 0.50,
+    padding: 0
+  },
+  pagination:{
+    
+  }
 });
