65d2340fe4ff2f7a4d49306df8e03c6a7fefb7e5
EmanSalah95 <seman9918@gmail.com>
2022-03-04 04:54:00 +0200

    search input

--- src/components/SearchModal/SearchModal.js
+++ src/components/SearchModal/SearchModal.js
@@ -3,18 +3,21 @@ import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-paper';
 import { ModalContext } from '../../context';
 import { styles } from '../../styles/searchStyles';
+import SearchInput from './SearchInput';
 
 export default function SearchModal() {
-  const { isSearchVisible, setSearchVisible}=useContext(ModalContext)
+  const { isSearchVisible, setSearchVisible}=useContext(ModalContext);
+  const closeModal=() => {
+    setSearchVisible(false);
+  }
   return (
     <Modal
     animationType="slide"
     visible={isSearchVisible}
-    onRequestClose={() => {
-      setSearchVisible(false);
-    }}
+    onRequestClose={closeModal}
   >
-    <View style={styles.centeredView}>
+    <View style={styles.container}>
+      <SearchInput closeModal={closeModal}/>
       
     </View>
   </Modal>
--- src/styles/searchStyles.js
+++ src/styles/searchStyles.js
@@ -1,10 +1,23 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    centeredView: {
-        flex: 1,
-        backgroundColor:'#fff',
-        padding:15,
-      },
+  container: {
+    backgroundColor:'#fff',
+    padding:15,
+    flex:1
+  },searchBox: {
+    borderRadius:25,
+    borderColor:'lightgray',
+    borderWidth:1,
+    flexDirection:'row',
+  },
+  back:{
+    width:50,
+    height:50,
+  },
+  input:{
+    width:'80%',
+    paddingHorizontal:10,
+  }
 
 });
