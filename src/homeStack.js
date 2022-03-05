8408310a98d4213ca91c37b7bee537354d1610eb
raghda244 <raghdamohsen244@gmail.com>
2022-03-05 06:14:36 +0200

    add product page

--- src/homeStack.js
+++ src/homeStack.js
@@ -4,15 +4,47 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Product from './screens/Product.js/product';
 import Products from './screens/Products/Products';
 import Tabs from './tabs';
+import { Text, TouchableOpacity } from 'react-native';
+import { ProductDetails } from './screens/Product.js/productDetails';
 
 const Tab = createMaterialBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+// const { favourits } = useSelector((state) => state.favourits);
+// let found = favourits?.find((i) => i.id === item.id);
+// const [isFavourite, setIsFavourite] = useState(found ? true : false);
+// const dispatch = useDispatch();
+
+const toggleFavourite = () => {
+  // dispatch(
+  //   isFavourite
+  //     ? removeFromFav(item.id)
+  //     : addToFav({ id: item.id, productData: item.data() })
+  // );
+}
 export default function HomeStack() {
   return (
-    <Stack.Navigator screenOptions={{headerShown:false}} >
+    <Stack.Navigator screenOptions={{ 
+      headerShown: false ,
+      headerTitleAlign: 'center',
+      headerTitleStyle: {
+        fontSize: 18,
+      }
+      }} >
       {/* <Stack.Screen name='Home' component={Home} /> */}
       <Stack.Screen name='Home' component={Tabs} />
-      <Stack.Screen name='Product' component={Product} />
+      <Stack.Screen 
+      name='Product' 
+      component={Product} 
+      options={{
+          headerShown: true
+        }}/>
+      <Stack.Screen 
+      name='ProductDetails' 
+      component={ProductDetails}
+      options={{
+          headerShown: true
+        }}/>
       <Stack.Screen name='Products' component={Products} />
     </Stack.Navigator>
   );
