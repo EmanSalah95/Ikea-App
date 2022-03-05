4b07e0cda616c3f72aa7a9de431f4423486efc60
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-03-05 09:06:44 +0200

    Add drawer content

--- src/screens/Favorites/drawerContent.js
+++ src/screens/Favorites/drawerContent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 
 import {
   Avatar,
@@ -15,38 +15,68 @@ import {
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
+import { h, w } from '../../constants/dimentions';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-export default function DrawerContent(props) {
+export default function DrawerContent() {
   return (
     <View style={{ flex: 1 }}>
-      <DrawerContentScrollView {...props}>
-        <View style={styles.drawerContent}>
-          <View style={styles.userInfoSection}>
-            <View>
-              <Avatar.Image
-                source={{
-                  uri: 'https://st3.depositphotos.com/1037987/15097/i/600/depositphotos_150975580-stock-photo-portrait-of-businesswoman-in-office.jpg',
-                }}
-                size={50}
-              />
-              <View style={{ marginLeft: 15, flexDirection: 'column' }}>
-                <Title style={styles.title}>Heba Ghazaly</Title>
-                <Caption style={styles.caption}>@hebaghazali</Caption>
-              </View>
-            </View>
-
-            <View></View>
+      <DrawerContentScrollView>
+        <View
+          style={{
+            backgroundColor: '#0051ba',
+            height: h * 0.2,
+            justifyContent: 'flex-end',
+            paddingLeft: 15,
+            paddingBottom: 20,
+          }}
+        >
+          <Text style={{ color: '#fff', fontSize: 25 }}>Select List</Text>
+        </View>
+        <View>
+          <View
+            style={{
+              backgroundColor: '#f4f8f8',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              padding: 20,
+              marginVertical: 10,
+            }}
+          >
+            <Text style={{ color: '#0051ba', fontWeight: 'bold' }}>
+              Shopping list
+            </Text>
+            <Text style={{ color: '#0051ba', fontWeight: 'bold' }}>0</Text>
+          </View>
+          <View style={{ margin: 20, flexDirection: 'row' }}>
+            <FontAwesome
+              name='warning'
+              color='#8a8a8c'
+              size={15}
+              style={{ marginRight: 10 }}
+            />
+            <Text style={{ marginRight: 10 }}>
+              This list will be merged with an existing one when you log in with
+              your IKEA account
+            </Text>
           </View>
         </View>
       </DrawerContentScrollView>
-      <Drawer.Section style={styles.bottomDrawerSection}>
-        <DrawerItem
-          icon={({ color, size }) => (
-            <Icon name='exit-to-app' color={color} size={size} />
-          )}
-          label='Sign Out'
-          onPress={() => {}}
-        />
+      <Drawer.Section>
+        <View style={{ padding: 15 }}>
+          <Text style={{ paddingBottom: 15, paddingRight: 8 }}>
+            Log in to access all your shopping lists
+          </Text>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#0051ba',
+              padding: 18,
+              alignItems: 'center',
+            }}
+          >
+            <Text style={{ color: '#fff', fontWeight: 'bold' }}>LOG IN</Text>
+          </TouchableOpacity>
+        </View>
       </Drawer.Section>
     </View>
   );
