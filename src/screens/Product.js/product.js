297ea763aa5e15c67ceeac2626becaceb0a0723b
raghda244 <raghdamohsen244@gmail.com>
2022-03-05 16:56:03 +0200

    delete loader folder

--- src/screens/Product.js/product.js
+++ src/screens/Product.js/product.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import { Button, ActivityIndicator } from 'react-native-paper';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
 import { getCollection, getDocumentByID } from '../../services/firebase';
@@ -16,7 +16,8 @@ export default function Product({ route, navigation }) {
 
   const [product, setProduct] = useState({});
   const [similarProducts, setSimilarProducts] = useState([]);
-  
+  const [loader, setLoader] = useState(true);
+
   const { favourits } = useSelector((state) => state.favourits);
   let { cartProducts } = useSelector((state) => state.cartProducts);
 
@@ -25,7 +26,7 @@ export default function Product({ route, navigation }) {
 
   const [isFavourite, setIsFavourite] = useState(found ? true : false);
   const [inCart, setInCart] = useState(foundInCart ? true : false);
-  
+
   const dispatch = useDispatch();
   const toggleFavourite = () => {
     dispatch(
@@ -51,9 +52,9 @@ export default function Product({ route, navigation }) {
   const getSimilarProducts = () => {
     getCollection('Products', ['SubCategory', '==', product.SubCategory])
       .then((res) => {
-        return res.filter(prd=>prd.id!=route.params.id);
+        return res.filter(prd => prd.id != route.params.id);
       })
-      .then((res)=>{
+      .then((res) => {
         setSimilarProducts(res)
       })
       .catch((err) => console.log('error :', err));
@@ -84,47 +85,61 @@ export default function Product({ route, navigation }) {
       title: product.ProductName
     })
     getSimilarProducts();
+    setLoader(false);
   }, [product, isFavourite]);
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-      <View>
-        <SwiperFlatList
-          index={0}
-          showPagination
-          data={product.Images}
-          renderItem={({ item }) => (
-            <Image source={{ uri: item }} style={styles.ImageSize} />
-          )}
-          paginationActiveColor='grey'
-          paginationDefaultColor='silver'
-          key={route.params.id}
-        />
-      </View>
-      <InfoSection product={product}  navigation={navigation}/>
-      {similarProducts.length!=0 && (
-        <>
-        <Text style={styles.InfoContainer}>Similar products</Text>
-          <HorizontalProducts navigation={navigation} products={similarProducts} />
-        </>
-      )}
-      </ScrollView>
-      <View style={{backgroundColor:'white',width:w,height:h*0.1,alignItems:'center'}}>
-      <Button 
-      style={inCart?styles.addedTocart:styles.addToCartBtn}
-      onPress={addCart}
-      disabled={inCart}
-      key={route.params.id}
-      >
-        {
-          (inCart) && <Text style={{color:'white'}}>Added to bag </Text>
-        }
-        {
-          (!inCart) &&  <Text style={{color:'white'}}>Add to bag <Text style={{color:'lightblue'}}> EGP {product.Price}</Text></Text>
-        }
-      </Button>
-      </View>
+      {
+        (loader) && (
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <ActivityIndicator animating={true} color='blue' />
+          </View>
+        )
+      }
+      {
+        (!loader) && (
+          <>
+            <ScrollView>
+              <View>
+                <SwiperFlatList
+                  index={0}
+                  showPagination
+                  data={product.Images}
+                  renderItem={({ item }) => (
+                    <Image source={{ uri: item }} style={styles.ImageSize} />
+                  )}
+                  paginationActiveColor='grey'
+                  paginationDefaultColor='silver'
+                  key={route.params.id}
+                />
+              </View>
+              <InfoSection product={product} navigation={navigation} />
+              {similarProducts.length != 0 && (
+                <>
+                  <Text style={styles.InfoContainer}>Similar products</Text>
+                  <HorizontalProducts navigation={navigation} products={similarProducts} />
+                </>
+              )}
+            </ScrollView>
+            <View style={{ backgroundColor: 'white', width: w, height: h * 0.1, alignItems: 'center' }}>
+              <Button
+                style={inCart ? styles.addedTocart : styles.addToCartBtn}
+                onPress={addCart}
+                disabled={inCart}
+                key={route.params.id}
+              >
+                {
+                  (inCart) && <Text style={{ color: 'white' }}>Added to bag </Text>
+                }
+                {
+                  (!inCart) && <Text style={{ color: 'white' }}>Add to bag <Text style={{ color: 'lightblue' }}> EGP {product.Price}</Text></Text>
+                }
+              </Button>
+            </View>
+          </>
+        )
+      }
     </View>
   );
 }
