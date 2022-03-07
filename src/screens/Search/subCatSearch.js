58f41b41e1a07818f47eac1f4fcc79999f26aea7
EmanSalah95 <seman9918@gmail.com>
2022-03-08 00:23:41 +0200

    fix toucgable in search

--- src/screens/Search/subCatSearch.js
+++ src/screens/Search/subCatSearch.js
@@ -1,46 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import {Text, View, Image, ImageBackground} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import { getCollection } from '../../services/firebase';
-import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+// import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
-export default function SubCatSearch({navigation,item}) {
-    const [subCat, setSubCat] = useState([])
+export default function SubCatSearch({ navigation, item }) {
+  const [subCat, setSubCat] = useState([]);
 
-    const getSubCat = async ()=>{
-      console.log('in sub');
-    
-    getCollection('subCategory').then((res) => {
-      console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>',res.length);
-    }
+  const getSubCat = async () => {
+    console.log('in sub');
 
-    ).catch(err=>console.log(err))
-  
+    getCollection('subCategory')
+      .then((res) => {
+        console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', res.length);
+      })
+      .catch((err) => console.log(err));
+
     let categories = [];
-    results.forEach(res => {
+    results.forEach((res) => {
       categories.push({ id: res.id, data: res.data() });
       console.log(categories);
       console.log(res);
     });
-  
-   setSubCat([...categories])
-  }
+
+    setSubCat([...categories]);
+  };
 
   return (
-
-    <View style={{backgroundColor: "#F1EAF1",padding: 15,margin: 5,}}>
+    <View style={{ backgroundColor: '#F1EAF1', padding: 15, margin: 5 }}>
       <TouchableWithoutFeedback
         onPress={() => {
           navigation.navigate('Products', {
             screenTitle: item.data().Name,
             condition: ['SubCategory', '==', item.id],
-          })
-          }}
- >
-    <Image source={{ uri: item.data().Image }} style={{ height: 120,width:150 }} />
-    <Text style={{fontSize:15,marginTop:7}}>{item.data().Name}</Text>
-
-</TouchableWithoutFeedback>
-  </View>
-
-  )
+          });
+        }}
+      >
+        <View>
+          <Image
+            source={{ uri: item.data().Image }}
+            style={{ height: 120, width: 150 }}
+          />
+          <Text style={{ fontSize: 15, marginTop: 7 }}>{item.data().Name}</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
 }
