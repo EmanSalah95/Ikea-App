76c34c6ef2c6e5483827a0d6a05cb2152bdb1428
EmanSalah95 <seman9918@gmail.com>
2022-03-07 20:07:27 +0200

    fix modal

--- src/screens/Products/filterMenu.js
+++ src/screens/Products/filterMenu.js
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Button } from 'react-native-paper';
+import { useSelector ,useDispatch } from 'react-redux';
 
 export default function FilterMenu({condition}) {
+  let { allProducts } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  const getUniqueFilters=()=>{
+    if (allProducts.length>0) {
+    }
+  }
+
 
   useEffect(() => {
-
-  }, []);
+   console.log('effect................');
+  }, [allProducts]);
   return (
     <View>
       <Button
