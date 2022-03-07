a41bfef14af8bedebcf8996e57dde7f4b608304f
EmanSalah95 <seman9918@gmail.com>
2022-03-08 00:02:52 +0200

    filter UI

--- src/screens/Products/filterMenu.js
+++ src/screens/Products/filterMenu.js
@@ -1,30 +1,73 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { ScrollView, View, Text, TouchableHighlight } from 'react-native';
 import { Button } from 'react-native-paper';
-import { useSelector ,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { styles } from '../../styles/filterStyles';
+import FilterField from './filterField';
+import {capitalize} from '../../services/utilities'
 
-export default function FilterMenu({condition}) {
+export default function FilterMenu({ condition }) {
   let { allProducts } = useSelector((state) => state.products);
+  const [materials, setMaterials] = useState(null);
+  const [colors, setColors] = useState(null);
+
   const dispatch = useDispatch();
 
-  const getUniqueFilters=()=>{
-    if (allProducts.length>0) {
-    }
-  }
+  const getUniqueFilters = () => {
+    if (allProducts.length > 0) {
+      let colors = [];
+      let materials = [];
+      allProducts.forEach((product) => {
+        const { Color, Material } = product.data();
 
+        let foundColor = colors.find((col) => col === Color);
+        !foundColor && colors.push(Color);
+
+        let foundMaterial = materials.find((mat) => mat.id === Material);
+        !foundMaterial && Material != undefined && materials.push(Material);
+      });
+
+      console.log('all materials', materials);
+      setMaterials(materials);
+      setColors(colors);
+      console.log('all colors', colors);
+    }
+  };
 
   useEffect(() => {
-   console.log('effect................');
+    console.log('effect................');
+    getUniqueFilters();
   }, [allProducts]);
   return (
-    <View>
-      <Button
-        icon='camera'
-        mode='contained'
-        onPress={() => {}}
-      >
-        test
-      </Button>
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollContainer}>
+        <FilterField title='Sort'  />
+        {colors&&<FilterField title='color' options={colors} />}
+        {materials&&<FilterField title='material' options={materials}/>}
+        <FilterField />
+        <FilterField />
+        <FilterField />
+        <FilterField />
+        <FilterField />
+      </ScrollView>
+
+      <View style={styles.buttonsContainer}>
+        <Button
+          style={[styles.btn,styles.clear]}
+          mode='outlined'
+          onPress={() => console.log('Pressed')}
+        >
+         <Text style={{color:'#000'}}>Clear all</Text> 
+        </Button>
+        <Button
+          style={[styles.btn,styles.view]}
+          mode='contained'
+          onPress={() => console.log('Pressed')}
+        >
+          View
+        </Button>
+      </View>
     </View>
   );
 }
---
+++ src/styles/filterStyles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContainer: {
+    flex: 0.8,
+  },
+  buttonsContainer: {
+    flex: 0.2,
+    flexDirection:'row',
+    paddingHorizontal:5,
+    justifyContent:'space-between',
+    alignItems:'center',
+    borderTopColor: '#E2E2E2',
+    borderTopWidth: 1,
+  },
+  btn: {
+      borderRadius:50,
+      width:'48%'
+  },
+  clear: {
+      borderColor:'#000',
+      color:'#000'
+  },
+  view: {
+      backgroundColor:'#000'
+  },
+  fieldRow: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 5,
+    borderBottomColor: '#E2E2E2',
+    borderBottomWidth: 1,
+  },
+});
+;
