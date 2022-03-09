29905c8008afdb1b3070a6273aa78e9c84ebfd6a
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-03-10 00:43:59 +0200

    Fix getting favorites from user issue

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -150,13 +150,16 @@ export const addFavItemsToUser = async (userID, productID) => {
     }
   });
 
-  updateDoc(doc(fireStore, 'users', userID), {
-    FavItems: [productID, ...favItems],
-  })
-    .then(() => {
-      console.log('Favourite items added to current user');
+  if (!cartItems.includes(productID))
+    updateDoc(doc(fireStore, 'users', userID), {
+      FavItems: [productID, ...favItems],
     })
-    .catch(err => console.log('adding Favourite items to user ERROR: ' + err));
+      .then(() => {
+        console.log('Favourite items added to current user');
+      })
+      .catch(err =>
+        console.log('adding Favourite items to user ERROR: ' + err)
+      );
 };
 
 export const removeFavItemFromUser = async (userID, productID) => {
